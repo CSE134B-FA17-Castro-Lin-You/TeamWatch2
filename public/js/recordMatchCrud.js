5bb0204d6c9e444b88a0f1ec1d80296b0f466091
Vaiolis <harrisonllin@gmail.com>
2017-11-27 07:03:15 -0800

    CRUD for match stats

--- public/js/recordMatchCrud.js
+++ public/js/recordMatchCrud.js
@@ -48,63 +48,12 @@ function handleUpdate() {
   }
 }
 
-function handleDelete() {
-  "use strict";
-  if (confirm("Are you sure you want to delete these match stats?")) {
-    if (parseInt(id, 10) > 0) {
-      firebase.database().ref('/Games/GameId:' + id).set(null).then(function (res) {
-        window.location = "/teamstats.html";
-      });
-    } else {
-      alert('invalid id');
-    }
-  }
-}
-
 document.addEventListener("DOMContentLoaded", function (event) {
   "use strict";
   firebase.database().ref('/Globals').once('value').then(function (snapshot) {
     teamName = snapshot.child('TeamName');
+    id = snapshot.child('GameCounter').val();
+    document.getElementById('us').innerHTML = teamName.val();
+    firebase.database().ref('/Globals/GameCounter').set(parseInt(id, 10) + 1);
   });
-  
-  var url = new URL(window.location.href);
-  id = url.searchParams.get("id");
-  if (parseInt(id, 10) > 0) {
-    firebase.database().ref('/Games/GameId:' + id).once('value').then(function (snapshot) {
-      if (!snapshot.exists()) {
-        alert('Not a recorded game');
-        window.location = "/view-game-schedule.html";
-      }
-
-      var datetime = snapshot.child('datetime'),
-        location = snapshot.child('location'),
-        type = snapshot.child('gameType'),
-        us = snapshot.child('stats').child('us'),
-        them = snapshot.child('stats').child('them'),
-        themName = snapshot.child('them'),
-        usCtr = 3,
-        themCtr = 4,
-        inputs = document.querySelectorAll('.form-control');
-
-      inputs[0].value = datetime.val();
-      inputs[1].value = location.val();
-      inputs[2].value = type.val();
-      
-      us.forEach(function (childSnapshot) {
-        inputs[usCtr].value = childSnapshot.val();
-        usCtr += 2;
-      });
-      
-      them.forEach(function (childSnapshot) {
-        inputs[themCtr].value = childSnapshot.val();
-        themCtr += 2;
-      });
-      
-      document.getElementById('them').innerHTML = themName.val();
-      document.getElementById('us').innerHTML = teamName.val();
-    });
-  } else {
-    alert('Not a valid game');
-    window.location = "/view-game-schedule.html";
-  }
 });
