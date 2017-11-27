61786d6ab03db3a9f426d6d5f37bda94de29610a
Han <hansol.you@innos-group.com>
2017-11-27 05:29:15 -0800

    game crud
    ..?

--- public/js/editGame.js
+++ public/js/editGame.js
@@ -4,34 +4,45 @@
 /*global firebase:true*/
 /*eslint no-undef: "error"*/
 /*eslint no-unused-vars: ["error", { "vars": "local", "args": "none" }]*/
+  document.addEventListener("DOMContentLoaded", function (event){
+    "use strict";
+    var url = new URL(window.location.href),
+    id = url.searchParams.get("id");
+    //console.log("parse id is " + id);
+    if(parseInt(id) > 0){
+      //console.log('/Games/GameId:' + id);
+      firebase.database().ref('/Games/GameId:' + id).once('value').then(function (snapshot){
+        if(!snapshot.exists()){
+          alert('No recorded game. id = ' + id);
+          window.location = "/view-game-schedule.html";
+        }
 
-document.addEventListener("DOMContentLoaded", function (event){
-  "use strict";
-  var url = new URL(window.location.href),
-  id = url.searchParams.get("id");
-  console.log("parse id is " + id);
-  if(parseInt(id) > 0){
-    console.log('/Games/GameId:' + id);
-    firebase.database().ref('/Games/GameId:' + id).once('value').then(function (snapshot){
-      if(!snapshot.exists()){
-        alert('No recorded game. id = ' + id);
-        window.location = "/view-game-schedule.html";
-      }
-      
-      var gDate = snapshot.child('gDate'),
-          location = snapshot.child('location'),
-          gTime = snapshot.child('gTime'),
-          gameType = snapshot.child('gameType'),
-          them = snapshot.child('them'),
-          status = snapshot.child('status'),
-          inputs = document.querySelectorAll('.form-control');
-      
-        inputs[0].value = them.val();
-        inputs[1].value = location.val();
-      
-    });
-  } else{
-    alert('Not a valid game');
+        var gDate = snapshot.child('gDate'),
+            location = snapshot.child('location'),
+            gTime = snapshot.child('gTime'),
+            // gameType = snapshot.child('gameType'),
+            them = snapshot.child('them'),
+            // status = snapshot.child('status'),
+            inputs = document.querySelectorAll('.form-control');
+
+          inputs[0].value = them.val();
+          inputs[1].value = location.val();
+          inputs[2].value = gDate.val();
+          inputs[3].value = gTime.val();
+
+      });
+    } else{
+      alert('Not a valid game');
+      window.location = "/view-game-schedule.html"
+    }
+  });
+function handleDeleteGame(){
+  document.addEventListener("DOMContentLoaded", function (event){
+    "use strict";
+    var url = new URL(window.location.href),
+    id = url.searchParams.get("id");
+    console.log("id = " + id);
+    firebase.database().ref('/Games/GameId:' + id).remove();
     window.location = "/view-game-schedule.html"
-  }
-});
+  });
+}
