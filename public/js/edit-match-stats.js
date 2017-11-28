25a28c3b681c7bd64ae5ac92cdce316bfd5a1ffe
Han <hansol.you@innos-group.com>
2017-11-28 04:24:28 -0800

    Touch up
    Touch up on consistency and mock data looking as realistic as it can.

--- public/js/edit-match-stats.js
+++ public/js/edit-match-stats.js
@@ -18,26 +18,28 @@ function handleUpdate() {
       datetime: inputs[0].value,
       location: inputs[1].value,
       gameType: inputs[2].value,
+      status: inputs[3].value,
+      them: inputs[4].value,
       stats: {
         us: {
-          "0-foul": parseInt(inputs[3].value, 10),
-          "1-red-card": parseInt(inputs[5].value, 10),
-          "2-yellow-card": parseInt(inputs[7].value, 10),
-          "3-shot-on-goal": parseInt(inputs[9].value, 10),
-          "4-goal": parseInt(inputs[11].value, 10),
-          "5-corner-kick": parseInt(inputs[13].value, 10),
-          "6-goal-kick": parseInt(inputs[15].value, 10),
-          "7-p-time": inputs[17].value
+          "0-foul": parseInt(inputs[5].value, 10),
+          "1-red-card": parseInt(inputs[7].value, 10),
+          "2-yellow-card": parseInt(inputs[9].value, 10),
+          "3-shot-on-goal": parseInt(inputs[11].value, 10),
+          "4-goal": parseInt(inputs[13].value, 10),
+          "5-corner-kick": parseInt(inputs[15].value, 10),
+          "6-goal-kick": parseInt(inputs[17].value, 10),
+          "7-p-time": inputs[19].value
         },
         them: {
-          "0-foul": parseInt(inputs[4].value, 10),
-          "1-red-card": parseInt(inputs[6].value, 10),
-          "2-yellow-card": parseInt(inputs[8].value, 10),
-          "3-shot-on-goal": parseInt(inputs[10].value, 10),
-          "4-goal": parseInt(inputs[12].value, 10),
-          "5-corner-kick": parseInt(inputs[14].value, 10),
-          "6-goal-kick": parseInt(inputs[16].value, 10),
-          "7-p-time": inputs[18].value
+          "0-foul": parseInt(inputs[6].value, 10),
+          "1-red-card": parseInt(inputs[8].value, 10),
+          "2-yellow-card": parseInt(inputs[10].value, 10),
+          "3-shot-on-goal": parseInt(inputs[12].value, 10),
+          "4-goal": parseInt(inputs[14].value, 10),
+          "5-corner-kick": parseInt(inputs[16].value, 10),
+          "6-goal-kick": parseInt(inputs[18].value, 10),
+          "7-p-time": inputs[20].value
         }
       }
     }).then(function (res) {
@@ -77,18 +79,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
       }
 
       var datetime = snapshot.child('datetime'),
+        status = snapshot.child('status'),
         location = snapshot.child('location'),
         type = snapshot.child('gameType'),
+        themName = snapshot.child('them'),
         us = snapshot.child('stats').child('us'),
         them = snapshot.child('stats').child('them'),
-        themName = snapshot.child('them'),
-        usCtr = 3,
-        themCtr = 4,
+        usCtr = 5,
+        themCtr = 6,
         inputs = document.querySelectorAll('.form-control');
 
       inputs[0].value = datetime.val();
       inputs[1].value = location.val();
       inputs[2].value = type.val();
+      inputs[3].value = status.val();
+      inputs[4].value = themName.val();
       
       us.forEach(function (childSnapshot) {
         inputs[usCtr].value = childSnapshot.val();
--- public/js/recordMatchCrud.js
+++ public/js/recordMatchCrud.js
@@ -19,18 +19,9 @@ function handleUpdate() {
       location: inputs[1].value,
       gameType: inputs[2].value,
       status: inputs[3].value,
+      them: inputs[4].value,
       stats: {
         us: {
-          "0-foul": parseInt(inputs[4].value, 10),
-          "1-red-card": parseInt(inputs[6].value, 10),
-          "2-yellow-card": parseInt(inputs[8].value, 10),
-          "3-shot-on-goal": parseInt(inputs[10].value, 10),
-          "4-goal": parseInt(inputs[12].value, 10),
-          "5-corner-kick": parseInt(inputs[14].value, 10),
-          "6-goal-kick": parseInt(inputs[16].value, 10),
-          "7-p-time": inputs[18].value
-        },
-        them: {
           "0-foul": parseInt(inputs[5].value, 10),
           "1-red-card": parseInt(inputs[7].value, 10),
           "2-yellow-card": parseInt(inputs[9].value, 10),
@@ -39,6 +30,16 @@ function handleUpdate() {
           "5-corner-kick": parseInt(inputs[15].value, 10),
           "6-goal-kick": parseInt(inputs[17].value, 10),
           "7-p-time": inputs[19].value
+        },
+        them: {
+          "0-foul": parseInt(inputs[6].value, 10),
+          "1-red-card": parseInt(inputs[8].value, 10),
+          "2-yellow-card": parseInt(inputs[10].value, 10),
+          "3-shot-on-goal": parseInt(inputs[12].value, 10),
+          "4-goal": parseInt(inputs[14].value, 10),
+          "5-corner-kick": parseInt(inputs[16].value, 10),
+          "6-goal-kick": parseInt(inputs[18].value, 10),
+          "7-p-time": inputs[20].value
         }
       }
     }).then(function (res) {
